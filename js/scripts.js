c9d985140b5406653838f9f89d6d362425706550
Brendan Telzrow <telzrowb@gmail.com>
2015-08-21 05:55:26 -0500

    added bootstrap, fix generator function, alert if no team selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,20 @@
 $(document).ready(function(){
-var $form = $('#radio');
-var teams = [];
-var classArray = [
-    "Allan",
-    "Ben",
-    "Brendan",
-    "Brook",
-    "Garrett",
-    "Jacob",
-    "James",
-    "Jason S.",
-    "Jason N.",
-    "Kathryn",
-    "Kim",
-    "Kamie",
-    "Katie",
-    "Liz",
-    "Madeleine",
-    "Martha",
-    "Matthew",
-    "Shawn",
-    "Vas"
-];
+	$form = $('#radio');
 
-var teamObject = {};
+var source   = $("#teams-template").html();
+var template = Handlebars.compile(source);
 
+	$form.on('submit', function(e){
+		e.preventDefault();
+		var numOfTeams = $('input:radio:checked').val();
+		if(numOfTeams == null) {
+			alert("You need to select a group size!")
+		}
+		var teams = teamGenerator(numOfTeams);
 
-$form.on('submit', function(e){
-  e.preventDefault();
-  var numOfTeams = $('input:radio:checked').val();
-  teamGenerator(numOfTeams);
-  teamObject = {
-  team1: teams[0],
-  team2: teams[1],
-  team3: teams[2],
-  team4: teams[3],
-  team5: teams[4],
-  team6: teams[5],
-  team7: teams[6],
-  team8: teams[7],
-  team9: teams[8],
-  team10: teams[9]
-}
-});
-
+		
+		$('#content').html(template(teams));
+	});
 
 
 //shuffle function!
@@ -65,26 +36,72 @@ function shuffle(array) {
   return array;
 }
 
+//constructor for team object
+function teamObject(name, group){
+	this.name = name;
+	this.group = group;
+	this.grouplist = function(){
+		this.group.join();
+	}
 
-
-
+};
 
 //team generating function 
 function teamGenerator(numOfTeams){
-  var shuffledClass = shuffle(classArray);
-  var teamSize = Math.floor(shuffledClass.length/numOfTeams);
-  if(numOfTeams == 10){teamSize = 2;}
-  for(var i=0; i<numOfTeams; i++){
-    teams.push(shuffledClass.splice(0,teamSize));
-  }
-  if(shuffledClass.length !== 0){
-    teams[numOfTeams-1].push(shuffledClass[0]);
-  }
+		var classArray = [
+    " Allan",
+    " Ben",
+    " Brendan",
+    " Brook",
+    " Garrett",
+    " Jacob",
+    " James",
+    " Jason S.",
+    " Jason N.",
+    " Kathryn",
+    " Kim",
+    " Kamie",
+    " Katie",
+    " Liz",
+    " Madeleine",
+    " Martha",
+    " Matthew",
+    " Shawn",
+    " Vas"
+];
+	//define variables
+		var currentTeam = [];
+		var allTeams = [];
+	//shuffle the class array	
+		var shuffledClass = shuffle(classArray);
+	//determine size of teams
+		var teamSize = Math.round(shuffledClass.length/numOfTeams);
+	//hard code team size to 2 if number of teams == 10
+	  // if(numOfTeams == 10){teamSize = 2;}
+	  console.log(teamSize);
+	//loop through
+	  for(var i=0; i<numOfTeams; i++){
+	  	//push selected team name to team array
+	  	var teamName = "Team " + (i+1);
+	  	//push selected person name to current team array
+	    currentTeam.push(shuffledClass.splice(0,teamSize));
+	    if(numOfTeams == 8){
+	    	if(shuffledClass.length < 10 && shuffledClass.length !== 0){
+	    		currentTeam[i].push(shuffledClass[0]);
+	    		shuffledClass.shift();
+	    	}
+	    }
+	        if(shuffledClass.length == 1){
+		    	currentTeam[i].push(shuffledClass[0]);
+		    	console.log("PUSHING EXTRA")
+		    	shuffledClass.shift();	
+		    }
 
-
-}
-
-
-
+	    var teamObj = new teamObject(teamName, currentTeam[i]);
+	    allTeams.push(teamObj);
+	  }
+	  //returns an array of team objects containing team name and members
+	  return allTeams;
+};
 
 });
